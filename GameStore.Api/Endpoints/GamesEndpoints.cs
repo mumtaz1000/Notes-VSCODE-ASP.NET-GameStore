8f47db8b3668efc32ad9d45ef12e4abf6f298bd2
mumtaz1000 <shereenfatima1000@gmail.com>
2024-01-23 00:11:04 +0100

    C08: Errors removed. No learning dependency injections

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,60 @@
+using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
+
+namespace GameStore.Api.Endpoints;
+public static class GamesEndpoints
+{
+    const string GetGameEnpointName = "GetGame";
+    public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
+    {
+        InMemGamesRepository repository = new();
+        var group = routes.MapGroup("/games").WithParameterValidation();
+        group.MapGet("/", () => repository.GetAll());
+
+        group.MapGet("/{id}", (int id) =>
+            {
+                Game? game = repository.Get(id);
+                return game is not null ? Results.Ok(game) : Results.NotFound();
+            })
+            .WithName(GetGameEnpointName);
+
+        group.MapPost("/", (Game game) =>
+            {
+                repository.Create(game);
+
+                return Results.CreatedAtRoute(GetGameEnpointName, new { id = game.Id }, game);
+            });
+
+        group.MapPut("/{id}", (int id, Game updatedGame) =>
+            {
+                Game? existingGame = repository.Get(id);
+
+                if (existingGame is null)
+                {
+                    return Results.NotFound();
+                }
+
+                existingGame.Name = updatedGame.Name;
+                existingGame.Genre = updatedGame.Genre;
+                existingGame.Price = updatedGame.Price;
+                existingGame.ReleaseDate = updatedGame.ReleaseDate;
+                existingGame.ImageUri = updatedGame.ImageUri;
+
+                repository.Update(existingGame);
+                return Results.NoContent();
+            });
+
+        group.MapDelete("/{id}", (int id) =>
+            {
+                Game? game = repository.Get(id);
+
+                if (game is not null)
+                {
+                    repository.Delete(id);
+                }
+
+                return Results.NoContent();
+            });
+            return group;
+    }
+}
