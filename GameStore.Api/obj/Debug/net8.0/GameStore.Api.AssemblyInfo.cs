7c42ce1405d29deb9d1416e67974a2d31f3401ba
mumtaz1000 <shereenfatima1000@gmail.com>
2024-01-23 21:30:35 +0100

    C15: Tutorial end

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79542bd81ee9d8ba9d19fbcd323f3d233c63d961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e28a17d7e162632ac864596ca50963c84bce1ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
