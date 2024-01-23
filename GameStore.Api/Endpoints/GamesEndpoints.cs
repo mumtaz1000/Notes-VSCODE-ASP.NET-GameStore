734402c6e79e3793416e0ddd8ef24cf810925c60
mumtaz1000 <shereenfatima1000@gmail.com>
2024-01-23 14:09:01 +0100

    C10: Adding sql server

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Dtos;
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
 
@@ -8,23 +9,31 @@ public static class GamesEndpoints
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/games").WithParameterValidation();
-        group.MapGet("/", (IGamesRepository repository) => repository.GetAll());
+        group.MapGet("/", (IGamesRepository repository) => 
+            repository.GetAll().Select( game => game.AsDto()));
 
         group.MapGet("/{id}", (IGamesRepository repository,int id) =>
             {
                 Game? game = repository.Get(id);
-                return game is not null ? Results.Ok(game) : Results.NotFound();
+                return game is not null ? Results.Ok(game.AsDto()) : Results.NotFound();
             })
             .WithName(GetGameEnpointName);
 
-        group.MapPost("/", (IGamesRepository repository, Game game) =>
+        group.MapPost("/", (IGamesRepository repository, CreateGameDto gameDto) =>
             {
+                Game game = new()
+                {
+                    Name = gameDto.Name,
+                    Genre = gameDto.Genre,
+                    Price = gameDto.Price,
+                    ReleaseDate = gameDto.ReleaseDate,
+                    ImageUri = gameDto.ImageUri
+                };
                 repository.Create(game);
-
                 return Results.CreatedAtRoute(GetGameEnpointName, new { id = game.Id }, game);
             });
 
-        group.MapPut("/{id}", (IGamesRepository repository, int id, Game updatedGame) =>
+        group.MapPut("/{id}", (IGamesRepository repository, int id, UpdateGameDto updatedGameDto) =>
             {
                 Game? existingGame = repository.Get(id);
 
@@ -33,11 +42,11 @@ public static class GamesEndpoints
                     return Results.NotFound();
                 }
 
-                existingGame.Name = updatedGame.Name;
-                existingGame.Genre = updatedGame.Genre;
-                existingGame.Price = updatedGame.Price;
-                existingGame.ReleaseDate = updatedGame.ReleaseDate;
-                existingGame.ImageUri = updatedGame.ImageUri;
+                existingGame.Name = updatedGameDto.Name;
+                existingGame.Genre = updatedGameDto.Genre;
+                existingGame.Price = updatedGameDto.Price;
+                existingGame.ReleaseDate = updatedGameDto.ReleaseDate;
+                existingGame.ImageUri = updatedGameDto.ImageUri;
 
                 repository.Update(existingGame);
                 return Results.NoContent();
